d48761b233ed20b6bfa547d6a1f70ebee2749721
specialdoom <bledea.bogdan97@gmail.com>
2023-12-05 18:15:37 +0000

    chore: use `preline.js` from cdn

--- src/lib/shared/components/layout/Sidebar.svelte
+++ src/lib/shared/components/layout/Sidebar.svelte
@@ -90,14 +90,6 @@
 					🎁 Children
 				</a>
 			</li>
-			<li>
-				<a
-					class="flex items-center gap-x-3.5 py-2 px-2.5 bg-gray-100 text-sm text-slate-700 rounded-lg hover:bg-gray-100 dark:bg-gray-900 dark:text-white dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-					href="/cookie-tracker"
-				>
-					🍪 Cookie Tracker
-				</a>
-			</li>
 		</ul>
 	</nav>
 </div>
